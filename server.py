56e1782aa350339292abd85923ae6ec51454cb41
Ted Vt iMac <58144503+BUSH222@users.noreply.github.com>
2023-04-02 19:46:37 +0300

    removed useless prints, pep8 formatting

--- server.py
+++ server.py
@@ -14,12 +14,12 @@ def checksavedposs(threshold=-80):
                 tempc[i][key] = value
             elif int(value) > threshold:
                 tempc[i][key] = value
-    print(tempc)
     tempc = list(filter(lambda x: len(x) > 2, tempc))
 
     with open('savedposs.txt', 'w') as file1:
         for k in tempc:
             print(json.dumps(k), file=file1)
+
 
 @app.route('/testdata', methods=['GET', 'POST'])
 def testdata():
@@ -27,16 +27,19 @@ def testdata():
     with open('savedposs.txt', 'a') as funfile:
         print(json.dumps(fundata), file=funfile)
     return 'received'
-    
+
+
 @app.route('/downloadapk')
 def downloadFile():
     return redirect("https://github.com/BUSH222/Audioloc/releases/tag/v0.1", code=302)
+
 
 @app.route('/mobileinfo')
 def mobileinfo():
     return open('instructions.html').read()
 
-@app.route('/analyse', methods=['POST']) #10.0.2.2:22222
+
+@app.route('/analyse', methods=['POST'])  # 10.0.2.2:22222
 def analyse():
     fundata = {}
 
@@ -53,10 +56,11 @@ def analyse():
                 a.pop('FNAME')
                 a.pop('NUM')
                 fundata[number] = a
-    
+
     comp_to = json.loads(request.data)
 
-    probs = dict(zip(fundata, len(fundata)*[0])) #magic, no clue how any of this works
+    # magic, no clue how any of this works
+    probs = dict(zip(fundata, len(fundata)*[0]))
     for num, d in fundata.items():
         for b, r in d.items():
             if b in comp_to.keys():
@@ -67,12 +71,12 @@ def analyse():
 
     return res_out[maxv[0]]
 
+
 @app.route('/')
 def index():
-  return "<html><body><h1>Welcome. Website is running.</h1></body></html>"
+    return "<html><body><h1>Welcome. Website is running.</h1></body></html>"
 
 
 if __name__ == "__main__":
     checksavedposs()
-    print('Check complete')
     app.run(host='0.0.0.0', port=22222)
