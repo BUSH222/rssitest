1d32f362bb2d232cc507973009191e8087e74bc1
Ted V <58144503+BUSH222@users.noreply.github.com>
2023-04-09 00:15:25 +0300

    better database, templates

--- server.py
+++ server.py
@@ -1,52 +1,55 @@
-from flask import Flask, request, redirect, send_from_directory
+from flask import Flask, request, redirect, send_from_directory, render_template
 import json
 import os
 app = Flask(__name__)
 
 
-def checksavedposs(threshold=-80):
-    orfile = open('savedposs.txt', 'r').readlines()
+def check_saved_poss(threshold=-80):
+    orfile = json.load(open('savedposs.json'))
     tempc = []
+
     for i, s in enumerate(orfile):
-        g = json.loads(s)
         tempc.append(dict())
-        for key, value in g.items():
+        for key, value in s.items():
             if key in ['NUM', 'FNAME']:
                 tempc[i][key] = value
             elif int(value) > threshold:
                 tempc[i][key] = value
-    tempc = list(filter(lambda x: len(x) > 2, tempc))
 
-    with open('savedposs.txt', 'w') as file1:
-        for k in tempc:
-            print(json.dumps(k), file=file1)
+    tempc = list(filter(lambda x: len(x) > 2, tempc))
+    tempc = [{key:d[key] for key in sorted(d.keys(), key=len)} for d in tempc]
+
+    with open('savedposs.json', 'w') as f:
+        json.dump(tempc, f, ensure_ascii=False, indent=4)
 
 
 @app.route('/testdata', methods=['GET', 'POST'])
-def testdata():
-    fundata = json.loads(request.data)
-    with open('savedposs.txt', 'a') as funfile:
-        print(json.dumps(fundata), file=funfile)
+def test_data():
+    data = json.loads(request.data)
+    todump = json.load(open('savedposs.json')).append(data)
+    with open('savedposs.txt', 'w') as f:
+        json.dump(todump, f, ensure_ascii=False, indent=4)
     return 'received'
 
 
 @app.route('/downloadapk')
-def downloadFile():
+def download_file():
     return redirect("https://github.com/BUSH222/Audioloc/releases/latest", code=302)
 
 
 @app.route('/mobileinfo')
-def mobileinfo():
-    return open('instructions.html').read()
+def mobile_info():
+    return render_template('instructions.html')
 
 
 @app.route('/analyse', methods=['POST'])  # 10.0.2.2:22222
 def analyse():
-    fundata = {}
+    data = {}
 
     res_out = {}
-    with open('savedposs.txt', 'r') as funfile2:
-        for s in funfile2.readlines():
+    with open('savedposs.txt', 'r') as f:
+        poss = json.load(f)
+        for s in poss:
             if len(s) != 0:
                 a = json.loads(s.strip())
                 assert 'NUM' in a.keys()
@@ -56,13 +59,13 @@ def analyse():
                 res_out[number] = filename
                 a.pop('FNAME')
                 a.pop('NUM')
-                fundata[number] = a
+                data[number] = a
 
     comp_to = json.loads(request.data)
 
     # magic, no clue how any of this works
-    probs = dict(zip(fundata, len(fundata)*[0]))
-    for num, d in fundata.items():
+    probs = dict(zip(data, len(data)*[0]))
+    for num, d in data.items():
         for b, r in d.items():
             if b in comp_to.keys():
                 er = abs(abs(int(r))-abs(int(comp_to[b])))
@@ -82,9 +85,9 @@ def get_audio(fname):
 
 @app.route('/')
 def index():
-    return open('homepage.html').read()
+    return render_template('homepage.html')
 
 
 if __name__ == "__main__":
-    checksavedposs()
+    check_saved_poss()
     app.run(host='0.0.0.0', port=22222)
